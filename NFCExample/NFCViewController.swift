9121c53a5da7f17975ef31c252d7a69c55e3d16a
Charles <charjami@hotmail.com>
2020-01-21 12:07:04 +0800

    minor changes

--- NFCExample/NFCViewController.swift
+++ NFCExample/NFCViewController.swift
@@ -12,7 +12,8 @@ import CoreNFC
 class NFCViewController: UIViewController, NFCTagReaderSessionDelegate  {
     
     
-
+    @IBOutlet weak var message: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,21 +24,36 @@ class NFCViewController: UIViewController, NFCTagReaderSessionDelegate  {
     var session:NFCTagReaderSession?
     
     func beginScanning() {
-        session = NFCTagReaderSession(pollingOption: [.iso14443, .iso15693], delegate: self)
+        session = NFCTagReaderSession(pollingOption: .iso14443, delegate: self, queue: nil)
         session?.alertMessage = "Hold your iPhone near the ISO7816 tag to begin transaction."
         session?.begin()
     }
     
     func tagReaderSessionDidBecomeActive(_ session: NFCTagReaderSession) {
         print("Active")
+        DispatchQueue.main.async {
+            self.message.text = "Active\n"
+        }
     }
     
     func tagReaderSession(_ session: NFCTagReaderSession, didInvalidateWithError error: Error) {
         print(error.localizedDescription)
+        DispatchQueue.main.async {
+            self.message.text = error.localizedDescription
+        }
     }
     
     func tagReaderSession(_ session: NFCTagReaderSession, didDetect tags: [NFCTag]) {
-        print("Detected")
+        print("\(tags)")
+        if case let NFCTag.iso7816(tag) = tags.first! {
+          print(tag.identifier as NSData)
+        }
+        
+        DispatchQueue.main.async {
+            self.message.text! = "\(tags)\n"
+            
+        }
+        session.restartPolling()
     }
     
     
